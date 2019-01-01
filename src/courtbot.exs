d1ea7e94763025b7785ffdda140e6e82228f828d
Tyler Samples <samples.tm@gmail.com>
2019-01-01 13:35:52 -0700

    Adding base package source

---
+++ src/courtbot.exs
@@ -0,0 +1,68 @@
+use Mix.Config
+
+config :courtbot, CourtbotWeb.Endpoint,
+  load_from_system_env: false,
+  server: true,
+  root: ".",
+  version: Mix.Project.config()[:version],
+  http: [
+    port: 4000,
+    ip: {0, 0, 0, 0}
+  ]
+
+# Database Configuration.
+config :courtbot, Courtbot.Repo,
+  load_from_system_env: false,
+  ssl: false,
+  # Must be changed
+  url: "POSTGRES_URL",
+  pool_size: 10
+
+# ExTwilio config.
+config :ex_twilio,
+  # Must be changed
+  account_sid: "TWILIO_SID",
+  # Must be changed
+  auth_token: "TWILIO_TOKEN"
+
+config :rollbax,
+  access_token: "ROLLBAR_TOKEN",
+  enable_crash_reports: true,
+  environment: "production"
+
+config :courtbot, Courtbot,
+  locales: %{
+    # Must be changed
+    "en" => "COURTBOT_NUMBER"
+  },
+  court_url: "https://mycourts.idaho.gov/",
+  # Times are in UTC
+  # 5am MST
+  import_time: "0 11 * * *",
+  # 1pm MSTa
+  notify_time: "0 19 * * *",
+  importer: %{
+    file: "/tmp/test.csv",
+    type:
+      {:csv,
+       [
+         {:has_headers, true},
+         {:field_mapping,
+          [
+            :case_number,
+            :last_name,
+            :first_name,
+            nil,
+            nil,
+            nil,
+            {:date, "%-m/%e/%Y"},
+            {:time, "%k:%M"},
+            nil,
+            :county
+          ]}
+       ]}
+  }
+
+# Import our generated secrets.
+# This file is generated
+import_config "courtbot.secrets.exs"
---
+++ src/courtbot.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+COURTBOT_BINARY="/opt/app/bin/courtbot"
+COURTBOT_PORT=${PORT:-4000}
+COURTBOT_CONFIG=${CONFIG:-"/etc/courtbot/"}
+COURTBOT_DATA=${DATA_DIR:-"/tmp"}
+
+print_help() {
+  cat << EOL
+Usage: courtbot COMMAND
+
+Commands:
+  import    Import data into Courtbot
+  notify    Send out pending notifications
+  start     Start Courtbot
+  stop      Stop Courtbot
+  logs      View Courtbot logs
+  help      Print usage
+EOL
+}
+
+
+is_running?() {
+  docker top courtbot &>/dev/null
+}
+
+stop_courtbot() {
+  docker stop courtbot  &>/dev/null || true
+  docker rm -f courtbot &>/dev/null || true
+}
+
+case $1 in
+     "import")
+          docker exec courtbot $COURTBOT_BINARY import
+          ;;
+     "notify")
+          docker exec courtbot $COURTBOT_BINARY notify
+          ;;
+     "reset")
+          read -p "Are you sure? This will delete all subscription and imported data" -n 1 -r
+          echo
+          if [[ $REPLY =~ ^[Yy]$ ]]
+          then
+              docker exec courtbot $COURTBOT_BINARY reset
+          fi
+          ;;
+     "start")
+          stop_courtbot
+
+          docker pull codeforboise/courtbot:latest
+          docker run --log-driver=journald --restart=always --name courtbot -p $COURTBOT_PORT:4000 -v "$COURTBOT_CONFIG":/opt/app/etc/courtbot -v "$COURTBOT_DATA":/tmp codeforboise/courtbot:latest
+          ;;
+     "stop")
+          stop_courtbot
+          ;;
+
+     "logs")
+          journalctl CONTAINER_NAME=courtbot
+          ;;
+
+     "help")
+          print_help
+          ;;
+     *)
+          print_help
+          ;;
+esac
